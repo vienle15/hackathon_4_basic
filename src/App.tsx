203f42bb291ed2ef2cf237493f80d97e14c48ac1
vienle15 <tanvien93lt@gmail.com>
2023-10-05 11:43:22 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -14,18 +14,21 @@ function App() {
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">1</p>
+            <button className="delete-button">Delete</button>
           </div>
         </div>
         <div className="card">
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">2</p>
+            <button className="delete-button">Delete</button>
           </div>
         </div>
         <div className="card">
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">3</p>
+            <button className="delete-button">Delete</button>
           </div>
         </div>
       </div>
